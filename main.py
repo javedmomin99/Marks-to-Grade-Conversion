dddc200a700ce64426b0ed614c91196f827a6215
Mohammad Javed Momin <javedmomin99@gmail.com>
2021-11-05 18:38:12 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
+
+
+for student in student_scores:
+  score = student_scores[student]
+  if score > 90:
+    student_scores[student] = "Outstanding"
+  elif score > 80:
+    student_scores[student] = "Exceeds Expectation"
+  elif score > 70:
+    student_scores[student] = "Acceptable"
+  else:
+    student_scores[student] = "Fail"
+    
+print(student_scores)
+
+
+
+
